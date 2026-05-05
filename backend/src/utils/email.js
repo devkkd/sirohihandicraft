9529236e4895262ddcebc2288903bf5ffcd98c8b
devkkd <developmentkontentkraftdigital.com>
2026-05-05 13:23:20 +0530

    solve

--- backend/src/utils/email.js
+++ backend/src/utils/email.js
@@ -1,9 +1,10 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: parseInt(process.env.EMAIL_PORT),
-  secure: false,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4, // force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
